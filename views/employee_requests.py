3e3f396a7d4aa585f06a9d9cd941c927e20e3c43
Derek Malone <laxmalone@gmail.com>
2023-04-13 15:08:12 -0500

    session 2 complete

---
+++ views/employee_requests.py
@@ -0,0 +1,13 @@
+EMPLOYEES = [
+    {
+        "id": 1,
+        "name": "Dale Gribble",
+        "email": "governmentbad@texas.gov",
+        "hourly_rate": 18,
+    }
+]
+
+
+def get_all_employees():
+    """This function will return all employees"""
+    return EMPLOYEES
